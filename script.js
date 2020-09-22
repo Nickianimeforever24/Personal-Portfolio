c03f51f8ab2c1d9c4414ca93885e4e5ba617ac7d
CTP <ctp@CTPs-MBP.fios-router.home>
2020-09-22 17:10:43 -0400

    Change Pics and Hover Over code works

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const picture = document.querySelector("#picResize");
-var pictureUrl = picture.src;
-console.log(pictureUrl);
+const nickiHamilton = document.querySelector("#picResize");
+const vrGames = document.querySelector("#gameBuster");
+const siteSearch = document.querySelector("#websiteSearch");
 
-function showAnotherPic() 
+function showAnotherMePic() 
 {
-    // alert("hey it's me");
-    picture.src = "https://pbs.twimg.com/media/Ehzj3w7VgAMZbk5.jpg";
-    
+    nickiHamilton.src = "https://pbs.twimg.com/media/Ehzj3w7VgAMZbk5.jpg";
+}
+function showAnotherGamePic()
+{
+    vrGames.src = "https://media.pocketgamer.com/FCKEditorFiles/Zombieville(1).jpg";
+}
+function showAnotherWebsitePic()
+{
+    siteSearch.src = "https://www.branex.com/wp-content/uploads/2017/10/fangoria.jpg";
 }
 
-picture.addEventListener("mouseover", showAnotherPic);
-// Add another EventListener
-// if statement  (w3schools learn "mouseover" and lose focus or mouseaway) 
+
+function showOriginalMePic()
+{
+    nickiHamilton.src = "https://pbs.twimg.com/profile_images/1228908593690415104/_8dP2HBo.jpg";
+}
+function showOriginalGamePic()
+{
+    vrGames.src = "https://www.clipartkey.com/mpngs/m/15-151860_xbox-controller-clipart-at-getdrawings-game-controller-cartoon.png";
+}
+function showOriginalWebsitePic()
+{
+    siteSearch.src = "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/112246660/original/b4f5ccb5fed3f11346863e5d0cdc5003cb058c81/draw-flat-design-or-detailed-illustrations.png";
+}
+
+
+nickiHamilton.addEventListener("mouseover", showAnotherMePic);
+nickiHamilton.addEventListener("mouseout", showOriginalMePic);
+vrGames.addEventListener("mouseover", showAnotherGamePic);
+vrGames.addEventListener("mouseout", showOriginalGamePic);
+siteSearch.addEventListener("mouseover", showAnotherWebsitePic);
+siteSearch.addEventListener("mouseout", showOriginalWebsitePic);
